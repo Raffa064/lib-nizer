ffb816ca8231f4503ecb033cf88b44653fcac88e
Raffa064 <raffaddtg@gmail.com>
2025-05-02 09:47:45 -0300

    Fixed error message

--- src/err.cpp
+++ src/err.cpp
@@ -4,7 +4,7 @@ namespace nz {
 
 error::operator std::string() {
   // Ex: Unexpected error at 2:34 'fn:void': Invalid token
-  return err + " at \e[32m" + ref.source + "\e[0m: " + reason;
+  return err + " at " + ((std::string)ref) + ": " + reason;
 }
 
 } // namespace nz
